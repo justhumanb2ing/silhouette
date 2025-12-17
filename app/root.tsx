4fe3eab5f68cfc7f969aff78b6eb3f4bdcdfb3c4
coggiee <zentechie7@gmail.com>
2025-12-17 20:53:43 +0900

    🌈 Page Design v1

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Anton&family=Oswald:wght@200..700&family=IBM+Plex+Mono:wght@300;400;500;600&display=swap",
   },
 ];
 
