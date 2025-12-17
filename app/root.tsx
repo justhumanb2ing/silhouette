4dabbd397cd0d443c9dbff017b9ad10a1ddbd25a
coggiee <zentechie7@gmail.com>
2025-12-17 21:50:40 +0900

    ✨ Integrate Sentry for error tracking and performance monitoring, adding configuration for both client and server, and update dependencies for Sentry integration in the project.

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react-router";
 import {
   isRouteErrorResponse,
   Links,
@@ -94,6 +95,8 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
     details = error.message;
     stack = error.stack;
   }
+
+  Sentry.captureException(error);
 
   return (
     <main className="pt-16 p-4 container mx-auto">
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
+import { sentryReactRouter } from "@sentry/react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { intlayer, intlayerProxy } from "vite-intlayer";
 
-export default defineConfig({
+export default defineConfig(config => ({
   plugins: [
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
     intlayer(),
     intlayerProxy(),
-  ],
-});
+    sentryReactRouter({
+      org: "hwisik",
+      project: "silhouette",
+      authToken: process.env.SENTRY_AUTH_TOKEN
+    }, config)
+  ]
+}));
